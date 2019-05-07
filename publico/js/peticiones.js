d8f48e847d40e43f423183de605d0ce68477f643
Cristian Vasquez <desarrollo-web1@zk.com.co>
2019-05-07 10:46:42 -0500

    Primer comit para proyecto integrador

---
+++ publico/js/peticiones.js
@@ -0,0 +1,15 @@
+function hola()
+{
+	try{
+		document.getElementById('videosIns').addEventListener('click',function(){
+			
+			
+		});
+	} catch(err){
+		console.log(err);
+	}
+}
+
+window.onload = function(e){ 
+    hola();
+}
