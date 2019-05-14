a6f2ccd3e3c69f0ee39009b940794ac8c77478e4
Cristian Vasquez <desarrollo-web1@zk.com.co>
2019-05-14 15:18:45 -0500

    agregar el video de mantenimiento preventivo

--- vistas/videosInstalacion.php
+++ vistas/videosInstalacion.php
@@ -41,7 +41,7 @@
 			  	<div class="text-center" >
 				  	<strong>Video de instalación de Windows 10</strong>
 				  	<br>
-				  	<a href="../media/manu.docx" >Manual de la instalacion de Windows 10</a>
+				  	<a href="../media/manual_instalacion_windows_10.docx" >Manual de la instalacion de Windows 10</a>
 				  	<hr>
 				  	<iframe width="600" height="315" src="https://www.youtube.com/embed/CsmYhdvmqGQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 				</div>
@@ -61,7 +61,7 @@
 			  	<div class="text-center" >
 				  	<strong>Video de instalacion de Linux Ubuntu</strong>
 				  	<br>
-				  	<a href="#" >Manual de la instalacion de Ubuntu</a>
+				  	<a href="../media/instalacion_de_ubuntu_manual.docx" >Manual de la instalacion de Ubuntu</a>
 				  	<hr>
 				  	<iframe width="600" height="315" src="https://www.youtube.com/embed/xsHCL_EZNTk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 				</div>
